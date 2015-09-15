605441092344557896aa89ed34a63c980c2dc139
Julian K. Arni <jkarni@gmail.com>
2015-09-15 11:37:17 +0200

    Cleanup errorspec description of routing

--- servant-server/test/Servant/Server/ErrorSpec.hs
+++ servant-server/test/Servant/Server/ErrorSpec.hs
@@ -16,18 +16,22 @@ import           Test.Hspec.Wai
 import           Servant
 
 
--- 1) Check whether one or more endpoints have the right path. Otherwise return 404.
--- 2) Check whether the one of those have the right method. Otherwise return
--- 405. If so, pick the first. We've now commited to calling at most one handler. *
--- 3) Check whether the Content-Type is known. Otherwise return 415.
--- 4) Check whether that one deserializes the body. Otherwise return 400. If there
--- was no Content-Type, try the first one of the API content-type list.
--- 5) Check whether the request is authorized. Otherwise return a 401.
--- 6) Check whether the request is forbidden. If so return 403.
--- 7) Check whether the request has a known Accept. Otherwise return 406.
--- 8) Check whether Accept-Language, Accept-Charset and Accept-Encoding exist and
--- match. We can follow the webmachine order here.
--- 9) Call the handler. Whatever it returns, we return.
+-- The semantics of routing and handling requests should be as follows:
+--
+--    1) Check whether one or more endpoints have the right path. Otherwise
+--    return 404.
+--    2) Check whether the one of those have the right method. Otherwise return
+--    405. If so, pick the first. We've now commited to calling at most one
+--    handler.
+--    3) Check whether the Content-Type is known. Otherwise return 415.
+--    4) Check whether that one deserializes the body. Otherwise return 400. If
+--    there was no Content-Type, try the first one of the API content-type list.
+--    5) Check whether the request is authorized. Otherwise return a 401.
+--    6) Check whether the request is forbidden. If so return 403.
+--    7) Check whether the request has a known Accept. Otherwise return 406.
+--    8) Check whether Accept-Language, Accept-Charset and Accept-Encoding
+--    exist and match. We can follow the webmachine order here.
+--    9) Call the handler. Whatever it returns, we return.
 
 spec :: Spec
 spec = describe "HTTP Errors" $ do
