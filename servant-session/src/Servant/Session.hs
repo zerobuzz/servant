7513e4560579587fb4948f0f764678cc71a99350
Julian K. Arni <jkarni@gmail.com>
2015-10-13 15:01:01 +0200

    Switch to wai-session

--- servant-session/src/Servant/Session.hs
+++ servant-session/src/Servant/Session.hs
@@ -1,71 +1,27 @@
 {-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE PolyKinds                  #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE TypeOperators              #-}
 
-module Servant.Session (Session, sessionMiddleware, Cookie(unCookie)) where
+module Servant.Session (SSession) where
 
-import qualified Blaze.ByteString.Builder as Builder
-import           Data.ByteString          (ByteString)
-import           Data.ByteString.Char8    (pack)
 import           Data.Proxy               (Proxy (Proxy))
-import           Data.String              (IsString)
-import           Data.Typeable            (Typeable)
-import           Data.UUID                (toASCIIBytes)
-import           GHC.Generics             (Generic)
-import           GHC.TypeLits
 import           Network.Wai
 import           Servant.API              ((:>))
 import           Servant.Server.Internal
-import           System.Random
-import           Web.ClientSession
-import           Web.Cookie
+import qualified Data.Vault.Lazy as Vault
+import Network.Wai.Session
 
 
-data Session (sessKey :: Symbol)
+data SSession (m :: * -> *) (k :: *) (v :: *)
 
--- | A @UUID@ cookie.
-newtype Cookie = Cookie { unCookie :: ByteString }
-  deriving (Eq, Show, IsString, Monoid, Read, Typeable, Generic)
-
-instance ( KnownSymbol sessKey, HasServer sublayout
-         ) => HasServer (Session sessKey :> sublayout) where
-  type ServerT (Session sessKey :> sublayout) m
-    = (Key -> Maybe Cookie) -> ServerT sublayout m
-  route Proxy a = WithRequest
-        $ \request -> route (Proxy :: Proxy sublayout)
-        $ passToServer a (getCookie key request)
-      where
-        key = pack $ symbolVal (Proxy :: Proxy sessKey)
-
-sessionMiddleware :: Key -> SetCookie -> Middleware
-sessionMiddleware key setCookie app req respond
-  = case getCookie (setCookieName setCookie) req key of
-    Nothing -> do
-        newCookie <- mkCookie key setCookie
-        let renderedC = Builder.toByteString $ renderSetCookie newCookie
-        app (req { requestHeaders = ("Cookie", renderedC):requestHeaders req})
-            (respond . injectCookie renderedC)
-    Just _  -> app req respond
-
-
-getCookie :: ByteString -> Request -> Key -> Maybe Cookie
-getCookie sessKey req key = do
-    c <- lookup "Cookie" $ requestHeaders req
-    v <- lookup sessKey $ parseCookies c
-    Cookie <$> decrypt key v
-
-mkCookie :: Key -> SetCookie -> IO SetCookie
-mkCookie key sc = do
-  x <- randomIO
-  c <- encryptIO key (toASCIIBytes x)
-  return (sc { setCookieValue = c })
-
-injectCookie :: ByteString -> Response -> Response
-injectCookie c = mapResponseHeaders (("Set-Cookie", c) :)
+instance ( HasServer sublayout
+         ) => HasServer (SSession n k v :> sublayout) where
+  type ServerT (SSession n k v :> sublayout) m
+    = (Vault.Key (Session n k v) -> Maybe (Session n k v)) -> ServerT sublayout m
+  route Proxy a = WithRequest $ \request -> route (Proxy :: Proxy sublayout)
+        $ passToServer a (\(key :: Vault.Key (Session n k v)) -> Vault.lookup key $ vault request)
--- servant-session/test/Servant/SessionSpec.hs
+++ servant-session/test/Servant/SessionSpec.hs
@@ -20,8 +20,13 @@ import           Servant
 import           Test.Hspec         (Spec, context, describe, it, shouldBe,
                                      shouldSatisfy)
 import           Test.Hspec.Wai
-import           Web.ClientSession
+import qualified Data.Vault.Lazy as Vault
 import           Web.Cookie
+import qualified Data.Map as Map
+import Network.Wai.Session
+import Network.Wai.Session.Map
+import Data.IORef
+import Control.Monad.Trans.Except
 
 import Servant.Session
 
@@ -30,40 +35,48 @@ spec = describe "Servant.Session" . with server $ do
 
   context "the cookie is set" $ do
 
-    it "resets the cookie if the cookie cannot be decoded" $ do
-        x <- request methodGet "" [("Cookie", "test=val")] ""
-        liftIO $ simpleBody x `shouldSatisfy` (/= "Nothing")
+    it "has access to the cookie" $ do
+        x <- request methodGet "" [("Cookie", "test=const")] ""
+        liftIO $ simpleBody x `shouldSatisfy` (== "1")
 
 
   context "no cookie is set" $ do
-
-    it "returns a fresh one" $ do
-        resp <- request methodGet "" [] ""
-        liftIO $ simpleBody resp `shouldSatisfy` (/= "Nothing")
 
     it "one will be in the Set-Cookie header of the response" $ do
         resp <- request methodGet "" [] ""
         let Just c = parseSetCookie <$> lookup "Set-Cookie" (simpleHeaders resp)
         liftIO $ setCookieName c `shouldBe` setCookieName setCookieOpts
+        liftIO $ setCookieValue c `shouldBe` "const"
 
     it "adds SetCookie params" $ do
         resp <- request methodGet "" [] ""
+        liftIO $ print resp
         let Just c = parseSetCookie <$> lookup "Set-Cookie" (simpleHeaders resp)
         liftIO $ setCookieMaxAge c `shouldBe` setCookieMaxAge setCookieOpts
 
-type API = Session "test" :> Get '[OctetStream] (Maybe ByteString)
-
-instance MimeRender OctetStream (Maybe ByteString) where
-    mimeRender _ Nothing  = "Nothing"
-    mimeRender _ (Just x) = fromStrict x
-
+type API = SSession IO Int String :> Get '[JSON] String
 
 setCookieOpts :: SetCookie
 setCookieOpts = def { setCookieName = "test" , setCookieMaxAge = Just 300 }
 
+sessionMiddleware :: SessionStore IO Int a -> Vault.Key (Session IO Int a) -> Middleware
+sessionMiddleware s = withSession s "test" setCookieOpts
+
 server ::  IO Application
 server = do
-    (_, key) <- randomKey
-    let handler x = return (unCookie <$> x key)
-    return $ sessionMiddleware key setCookieOpts
-           $ serve (Proxy :: Proxy API) handler
+    ref <- mapStore (return "const")
+    key <- Vault.newKey
+    return $ sessionMiddleware ref key
+           $ serve (Proxy :: Proxy API) (handler key)
+
+handler :: Vault.Key (Session IO Int String)
+        -> (Vault.Key (Session IO Int String) -> Maybe (Session IO Int String))
+        -> ExceptT ServantErr IO String
+handler key map = do
+    x <- liftIO $ lkup 1
+    liftIO $ print ("Cookie:\t" ++ show x)
+    case x of
+        Nothing -> liftIO (ins 1 "a") >> return "Nothing"
+        Just y -> liftIO (ins 1 ('a':y)) >> return (show y)
+  where
+   Just (lkup, ins) = map key
